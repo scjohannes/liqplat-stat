8dfe88f72c04f57a77eb1ccc2717feda1467af05
aws-server <johannes.schwenke@gmail.com>
2025-08-14 10:29:31 +0000

    modified:   render-all.sh

--- render-all.sh
+++ render-all.sh
@@ -3,7 +3,7 @@ echo "--- Starting Quarto render process ---"
 
 quarto render 2-hosp/8.1-sim-resampling-markov-H0-no-int.qmd && \
 quarto render 2-hosp/8.2-sim-resampling-markov-H0-linear-int.qmd && \
-quarto render 2-hosp/8.3-sim-resampling-markov-H0-full-int.qmd
+quarto render 2-hosp/8.3-sim-resampling-markov-H0-full-int.qmd && \
 
 echo "--- All rendering complete. Shutting down now. ---"
 sudo shutdown -h now
