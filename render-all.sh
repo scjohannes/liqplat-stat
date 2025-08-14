e2d3ce1610eb2b96713edd1cd1618de4fcdb0b92
aws-server <johannes.schwenke@gmail.com>
2025-08-14 09:07:55 +0000

    modified:   .gitignore 	modified:   2-hosp/8.1-sim-resampling-markov-H0-no-int.qmd 	modified:   2-hosp/8.2-sim-resampling-markov-H0-linear-int.qmd 	modified:   2-hosp/8.3-sim-resampling-markov-H0-full-int.qmd 	modified:   render-all.sh

--- render-all.sh
+++ render-all.sh
@@ -6,3 +6,4 @@ quarto render 2-hosp/8.2-sim-resampling-markov-H0-linear-int.qmd
 quarto render 2-hosp/8.3-sim-resampling-markov-H0-full-int.qmd
 
 echo "--- All rendering complete. Shutting down now. ---"
+sudo shutdown -h now
