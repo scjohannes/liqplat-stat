423179eb74e7d7228b45066de355b8ecca7e5d50
aws-server <johannes.schwenke@gmail.com>
2025-08-19 14:12:26 +0000

    modified:   .gitignore 	modified:   2-hosp/8.4-sim-resampling-markov-H0-no-int-cluster.qmd 	modified:   2-hosp/9-sim-results.qmd 	modified:   render-all.sh

--- render-all.sh
+++ render-all.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 echo "--- Starting Quarto render process ---"
 
-quarto render 2-hosp/8.1-sim-resampling-markov-H0-no-int.qmd && \
-quarto render 2-hosp/8.2-sim-resampling-markov-H0-linear-int.qmd && \
-quarto render 2-hosp/8.3-sim-resampling-markov-H0-full-int.qmd && \
 quarto render 2-hosp/8.4-sim-resampling-markov-H0-no-int-cluster.qmd && \
 
 echo "--- Shutting Down Instance ---"
