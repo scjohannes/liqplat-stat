56d530c5daf31952e430f04bd7f673f763f32ef3
aws-server <johannes.schwenke@gmail.com>
2025-10-14 08:50:57 +0000

    modified:   .gitignore 	modified:   render-all.sh

--- render-all.sh
+++ render-all.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 echo "--- Starting Quarto render process ---"
 
-quarto render 2-hosp/10-power-or-09.qmd && \
-quarto render 2-hosp/10-power-or-08.qmd && \
 quarto render 2-hosp/10-power-or-07.qmd && \
 
 echo "--- Shutting Down Instance ---"
